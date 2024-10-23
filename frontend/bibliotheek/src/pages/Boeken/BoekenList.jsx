f457ed947d321ea7b21cac67dbfff9c1c56a9695
MaximBauwelinck1 <maxim.bauwelinck@student.hogent.be>
2024-10-23 21:19:26 +0200

    H1 toegevoegd

--- frontend/bibliotheek/src/pages/Boeken/BoekenList.jsx
+++ frontend/bibliotheek/src/pages/Boeken/BoekenList.jsx
@@ -3,19 +3,19 @@ import Boek from '../../components/boeken/boek';
 
 const BoekenList = () => {
   return (
-    <div className='grid mt-3'>
-      <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-3'>
+    <>
+      <h1>Boeken </h1>
+      <div className='grid'>
         {boeken
           .sort((a, b) =>
             a.titel.toUpperCase().localeCompare(b.titel.toUpperCase()),
           )
-          .map((p) => (
-            <div className='col' key={p.id}>
-              <Boek key={p.id}  {...p} />
-            </div>
+          .map((p) => (       
+            <Boek key={p.id}  {...p} />
           ))}
       </div>
-    </div>
+    </>
+    
   );
 };
 
