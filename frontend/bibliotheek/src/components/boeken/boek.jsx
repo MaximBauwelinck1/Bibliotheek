945afb4ab1834572f0d3890caba094845c641c93
MaximBauwelinck1 <maxim.bauwelinck@student.hogent.be>
2024-10-23 20:57:10 +0200

    frontend boek component aangepast

--- frontend/bibliotheek/src/components/boeken/boek.jsx
+++ frontend/bibliotheek/src/components/boeken/boek.jsx
@@ -1,8 +1,29 @@
+import '../../css/Boek.css';
+
 const Boek = (prop) => {
   return (
-    <div className='bg-light border-dark mb-4'>
-      <div className="text-bg-dark" style={{textAlign:'center'}}> 
-        {prop.naam} van autheur {prop.autheur} met genre {prop.genre} en beschrijving {prop.description}  
+    <div className="boek_container">
+      <h1 className="boek_titel">{prop.titel}</h1>
+
+      <div className="cover_container">
+        <div className="cover">
+          {prop.cover_uri ? <img src={prop.cover_uri} alt="Cover" className="cover_img" /> : 'Cover Image'}
+        </div>
+
+        <div className="auteur_info">
+          <div className="auteur_foto">
+            {prop.authorPhoto ? <img src={prop.authorPhoto} alt="Author" className="author_img" /> : 'Author Photo'}
+          </div>
+          <div className="auteur_description">
+            <h3>{prop.voornaam + prop.achternaam}</h3>
+            <p>{prop.biografie}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="description_container">
+        <h2>beschrijving</h2>
+        <p>{prop.beschrijving}</p>
       </div>
     </div>
   );
--- frontend/bibliotheek/src/pages/Boeken/BoekenList.jsx
+++ frontend/bibliotheek/src/pages/Boeken/BoekenList.jsx
@@ -1,14 +1,13 @@
-import BOEKEN_DATA from '../../api/mock_data';
+import  {boeken} from '../../api/mock_data';
 import Boek from '../../components/boeken/boek';
 
 const BoekenList = () => {
-  const boeken = BOEKEN_DATA;
   return (
     <div className='grid mt-3'>
       <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-3'>
         {boeken
           .sort((a, b) =>
-            a.name.toUpperCase().localeCompare(b.name.toUpperCase()),
+            a.titel.toUpperCase().localeCompare(b.titel.toUpperCase()),
           )
           .map((p) => (
             <div className='col' key={p.id}>
