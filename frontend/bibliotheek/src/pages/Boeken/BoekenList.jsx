661ad0c0873642f91012004d47d212a0d98d6b7f
MaximBauwelinck1 <maxim.bauwelinck@student.hogent.be>
2024-10-29 20:47:41 +0100

    filter laten werken

--- frontend/bibliotheek/src/pages/Boeken/BoekenList.jsx
+++ frontend/bibliotheek/src/pages/Boeken/BoekenList.jsx
@@ -7,6 +7,10 @@ const BoekenList = () => {
   const[boeken,setBoeken] = useState(BOEKEN_DATA);
   const [text, setText] = useState('');
   const [search, setSearch] = useState('');
+  const [categorie, setCategorie] = useState('');
+  const [searchcategorie, setSearchCategorie] = useState('');
+  const [taal, setTaal] = useState('');
+  const [searchtaal, seSearchtTaal] = useState('');
   return (
     <>
       <div className='text-center'>Bibliotheek</div>
@@ -20,7 +24,9 @@ const BoekenList = () => {
             placeholder='Zoeken'
             onChange={(e) => setText(e.target.value)}
           />
-          <select id="book-genre" name="genre" multiple size="3">
+          <select id="book-genre" name="genre" multiple size="3" onChange={(e) => {
+            setCategorie(Array.from(e.target.selectedOptions, (option) => option.value));
+          }}>
             <option value="Fictie">Fictie</option>
             <option value="Non-fictie">Non-fictie</option>
             <option value="Mysterie">Mysterie</option>
@@ -29,8 +35,16 @@ const BoekenList = () => {
             <option value="Biografie">Biografie</option>
             <option value="Romantiek">Romantiek</option>
             <option value="Geschiedenis">Geschiedenis</option>
+            <option value="Dystopisch">Dystopisch</option>
+            <option value="Southern Gothic">Southern Gothic</option>
+            <option value="Post-apocalyptisch">Post-apocalyptisch</option>
+            <option value="Tragedie">Tragedie</option>
+            <option value="Anti-War">Anti-War</option>
+            <option value="Aventuur">Aventuur</option>
           </select>
-          <select id="taal" name="taal" multiple size="3">
+          <select id="taal" name="taal" multiple size="3" onChange={(e) => {
+            setTaal(Array.from(e.target.selectedOptions, (option) => option.value));
+          }}>
             <option value="Engels">Engels</option>
             <option value="Nederlands">Nederlands</option>
             <option value="Frans">Frans</option>
@@ -38,9 +52,26 @@ const BoekenList = () => {
             <option value="Zweeds">Zweeds</option>
           </select>
           
-          <button type='button' className='btn btn-outline-primary' onClick={() => setSearch(text)}>
+          <button type='button' className='btn btn-outline-primary' onClick={() => {
+            setSearch(text);
+            setSearchCategorie(categorie);
+            seSearchtTaal(taal);
+          }}>
             Filter toepassen
           </button>
+          <span
+            style={{ marginLeft: '10px', cursor: 'pointer', color: 'blue', textDecoration: 'underline' }}
+            onClick={() => {
+              setText('');        
+              setCategorie([]);   
+              setTaal([]);        
+              setSearch('');      
+              setSearchCategorie([]);  
+              seSearchtTaal([]);       
+            }}
+          >
+            Alle filters verwijderen
+          </span>
         </div>
       </div>
       
@@ -48,7 +79,12 @@ const BoekenList = () => {
         {boeken
           .sort((a, b) =>
             a.titel.toUpperCase().localeCompare(b.titel.toUpperCase()),
-          ).filter((a) => a.titel.toLowerCase().includes(search.toLowerCase()))
+          ).filter((a) =>{
+            const matchedTitel = search ? a.titel.toLowerCase().includes(search.toLowerCase()) : true;
+            const MatchedGenre = searchcategorie.length > 0 ? searchcategorie.includes(a.genre) : true;
+            const MatchedTaal =  searchtaal.length > 0 ? searchtaal.includes(a.taal) : true;
+            return matchedTitel && MatchedGenre && MatchedTaal;
+          })
           .map((p) => (       
             <Boek key={p.id}  {...p} />
           ))}
