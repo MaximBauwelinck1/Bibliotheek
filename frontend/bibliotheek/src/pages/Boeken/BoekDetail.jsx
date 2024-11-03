d2f32667a35e981a7cdcb4a50162a727b30f2d57
MaximBauwelinck1 <maxim.bauwelinck@student.hogent.be>
2024-11-04 00:12:00 +0100

    Update BoekDetail.jsx

--- frontend/bibliotheek/src/pages/Boeken/BoekDetail.jsx
+++ frontend/bibliotheek/src/pages/Boeken/BoekDetail.jsx
@@ -14,40 +14,39 @@ const BoekDetail = () => {
     isLoading,
     error,
   } = useSWR( id?`boeken/${id}`: null, API.getById);
-  console.log(boek);
+  
   if (!boek) {
     return (
-      <div>
-        <h1 className='text-center' style={{ paddingTop: '10em'}}>Boek niet gevonden.</h1>
-        <p className='text-center'>Boek met id:{id} bestaat niet.</p>
-      </div>
+      <AsyncData loading={isLoading} error={error}>
+      </AsyncData>
     );
   }
 
   return (
     <>
       <Link to={'/boeken'}> <button className={styles.top_left_button} >Terugkeren</button></Link>
-      <AsyncData loading={isLoading} error={error}>
-        <div className={styles.boek_detail_container}>
+      
+      <div className={styles.boek_detail_container}>
        
-          <img src={boek.cover_uri} alt={boek.titel} className={styles.cover_img} />
+        <img src={boek.cover_uri} alt={boek.titel} className={styles.cover_img} />
  
-          <div className={styles.boek_info}>
-            <h1 className={styles.boek_titel}>{boek.titel}</h1>
-            <p><strong>Auteur:</strong> {boek.voornaam} {boek.achternaam}</p>
-            <p><strong>Genre:</strong> {boek.genre}</p>
-            <p><strong>Publicatiedatum:</strong> {boek.publicatie_datum}</p>
-            <p><strong>Taal:</strong> {boek.taal}</p>
-            <p><strong>Pagina&apos;s:</strong> {boek.paginas}</p>
-            <p><strong>Beschikbare Kopieën:</strong> {boek.vrije_kopieën} / {boek.totale_kopieën}</p>
-            <h3>Beschrijving</h3>
-            <p>{boek.beschrijving}</p>
-            <h3>Auteursinformatie</h3>
-            <p><strong>Nationaliteit:</strong> {boek.nationaliteit}</p>
-            <p><strong>Biografie:</strong> {boek.biografie}</p>
-          </div>
+        <div className={styles.boek_info}>
+          <h1 className={styles.boek_titel}>{boek.titel}</h1>
+          <p><strong>Auteur:</strong> {boek.auteur.voornaam} {boek.auteur.achternaam}</p>
+          <p><strong>Genre:</strong> {boek.genre}</p>
+          <p><strong>Publicatiedatum:</strong> {new Date(boek.publicatie_datum).
+            toLocaleDateString('nl-BE', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
+          <p><strong>Taal:</strong> {boek.taal}</p>
+          <p><strong>Pagina&apos;s:</strong> {boek.paginas}</p>
+          <p><strong>Beschikbare Kopieën:</strong> {boek.vrije_kopieen} / {boek.totale_kopieen}</p>
+          <h3>Beschrijving</h3>
+          <p>{boek.beschrijving}</p>
+          <h3>Auteursinformatie</h3>
+          <p><strong>Nationaliteit:</strong> {boek.auteur.nationaliteit}</p>
+          <p><strong>Biografie:</strong> {boek.auteur.biografie}</p>
         </div>
-      </AsyncData>
+      </div>
+      
     </>
   );
 };
