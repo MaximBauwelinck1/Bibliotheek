09c0de0ce2b43fc6d973a210bd313231116636dc
MaximBauwelinck1 <maxim.bauwelinck@student.hogent.be>
2024-11-04 10:35:00 +0100

    boek prop gebruiken bij boekdetail

--- frontend/bibliotheek/src/pages/Boeken/BoekDetail.jsx
+++ frontend/bibliotheek/src/pages/Boeken/BoekDetail.jsx
@@ -17,36 +17,18 @@ const BoekDetail = () => {
   
   if (!boek) {
     return (
-      <AsyncData loading={isLoading} error={error}>
-      </AsyncData>
+      <>
+        <Link to={'/boeken'}> <button className={styles.top_left_button} >Terugkeren</button></Link>  
+        <AsyncData loading={isLoading} error={error}>
+        </AsyncData>
+      </>
     );
   }
 
   return (
     <>
-      <Link to={'/boeken'}> <button className={styles.top_left_button} >Terugkeren</button></Link>
-      
-      <div className={styles.boek_detail_container}>
-       
-        <img src={boek.cover_uri} alt={boek.titel} className={styles.cover_img} />
- 
-        <div className={styles.boek_info}>
-          <h1 className={styles.boek_titel}>{boek.titel}</h1>
-          <p><strong>Auteur:</strong> {boek.auteur.voornaam} {boek.auteur.achternaam}</p>
-          <p><strong>Genre:</strong> {boek.genre}</p>
-          <p><strong>Publicatiedatum:</strong> {new Date(boek.publicatie_datum).
-            toLocaleDateString('nl-BE', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
-          <p><strong>Taal:</strong> {boek.taal}</p>
-          <p><strong>Pagina&apos;s:</strong> {boek.paginas}</p>
-          <p><strong>Beschikbare Kopieën:</strong> {boek.vrije_kopieen} / {boek.totale_kopieen}</p>
-          <h3>Beschrijving</h3>
-          <p>{boek.beschrijving}</p>
-          <h3>Auteursinformatie</h3>
-          <p><strong>Nationaliteit:</strong> {boek.auteur.nationaliteit}</p>
-          <p><strong>Biografie:</strong> {boek.auteur.biografie}</p>
-        </div>
-      </div>
-      
+      <Link to={'/boeken'}> <button className={styles.top_left_button} >Terugkeren</button></Link>     
+      <Boek key={id} {...boek}/>
     </>
   );
 };
