2a3a5f84a51169617aef3fe09df69d925ac0f2e0
MaximBauwelinck1 <maxim.bauwelinck@student.hogent.be>
2024-11-05 00:43:39 +0100

    Update navbar.jsx

--- frontend/bibliotheek/src/components/navbar.jsx
+++ frontend/bibliotheek/src/components/navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <NavLink to="/boeken" className={({ isActive }) => isActive ? styles.actieveLink : styles.link}>Boeken</NavLink>
-      <NavLink to="/about" className={({ isActive }) => isActive ? styles.actieveLink : styles.link}>About</NavLink>
+      <NavLink to="/about" className={({ isActive }) => isActive ? styles.actieveLink : styles.link}>Over ons</NavLink>
     </nav>
   );
 };
