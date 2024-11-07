3583fcf75f1825a57dd290d74dc9f5ab6c5e89de
MaximBauwelinck1 <maxim.bauwelinck@student.hogent.be>
2024-11-07 01:27:46 +0100

    zowel testen voor isbn 13 als 10

--- backend/__tests__/rest/boeken.spec.ts
+++ backend/__tests__/rest/boeken.spec.ts
@@ -199,7 +199,7 @@ describe('boeken', () => {
       await prisma.auteur.createMany({ data: data.auteurs });
     });
 
-    it('should be 201 and return the created book', async () => {
+    it('should be 201 and return the created book', async () => { //met isbn 13 code
       const response = await request.post(url).send({
         ISBN: '9780141439518',
         titel: 'Pride and Prejudice',
@@ -223,6 +223,39 @@ describe('boeken', () => {
       expect(response.status).toBe(201); 
       expect(response.body.id).toBeTruthy(); 
       expect(response.body.titel).toBe('Pride and Prejudice'); 
+      expect(response.body.cover_uri).toBe('https://example.com/cover/pride-and-prejudice.jpg'); 
+      expect(response.body.auteur).toEqual(expect.objectContaining({
+        voornaam: 'Gabriel',
+        achternaam: 'Garcia Marquez',
+        geboortedatum:'1927-03-06T00:00:00.000Z',
+        nationaliteit: 'Colombian',
+        biografie: 'Known for One Hundred Years of Solitude.',
+      }));
+    });
+    it('should be 201 and return the created book', async () => { // met isbn 10 code
+      const response = await request.post(url).send({
+        ISBN: '9781590302255',
+        titel: 'ISBN10',
+        genre: 'Romantiek',
+        publicatie_datum: new Date('1903-06-25T00:00:00.000Z'),
+        taal: 'Engels',
+        paginas: 279,
+        vrije_kopieen: 2,
+        totale_kopieen: 4,
+        beschrijving: 'The novel follows the character development of Elizabeth Bennet.',
+        cover_uri: 'https://example.com/cover/pride-and-prejudice.jpg',
+        auteur: {
+          voornaam: 'Gabriel',
+          achternaam: 'Garcia Marquez',
+          geboortedatum: new Date('1927-03-06T00:00:00.000Z'),
+          nationaliteit: 'Colombian',
+          biografie: 'Known for One Hundred Years of Solitude.',
+        },
+      });
+      
+      expect(response.status).toBe(201); 
+      expect(response.body.id).toBeTruthy(); 
+      expect(response.body.titel).toBe('ISBN10'); 
       expect(response.body.cover_uri).toBe('https://example.com/cover/pride-and-prejudice.jpg'); 
       expect(response.body.auteur).toEqual(expect.objectContaining({
         voornaam: 'Gabriel',
@@ -346,7 +379,7 @@ describe('boeken', () => {
       await prisma.boek.createMany({ data: data.boeken });
     });
 
-    it('should be 200 and return the updated book', async () => {
+    it('should be 200 and return the updated book', async () => { // met isbn 13
       const response = await request.put(url+'/3fa85f64-5717-4562-b3fc-2c963f66afa6').send({
         ISBN: '9783127323207',
         titel: 'test',
@@ -372,6 +405,40 @@ describe('boeken', () => {
       expect(response.body.titel).toBe('test'); 
       expect(response.body.cover_uri).toBe('https://example.com/cover/pride-and-prejudice.jpg'); 
       expect(response.body.ISBN).toBe('9783127323207');
+      expect(response.body.auteur).toEqual(expect.objectContaining({
+        voornaam: 'Gabriel',
+        achternaam: 'Garcia Marquez',
+        geboortedatum:'1927-03-06T00:00:00.000Z',
+        nationaliteit: 'Colombian',
+        biografie: 'Known for One Hundred Years of Solitude.',
+      }));
+    });
+    it('should be 200 and return the updated book', async () => { // met isbn 10 
+      const response = await request.put(url+'/5e846780-937b-471d-96e3-d567b86a95bb').send({
+        ISBN: '9781590302255',
+        titel: 'ISBN10',
+        genre: 'Romantiek',
+        publicatie_datum: new Date('1903-06-25T00:00:00.000Z'),
+        taal: 'Engels',
+        paginas: 279,
+        vrije_kopieen: 2,
+        totale_kopieen: 4,
+        beschrijving: 'The novel follows the character development of Elizabeth Bennet.',
+        cover_uri: 'https://example.com/cover/pride-and-prejudice.jpg',
+        auteur: {
+          voornaam: 'Gabriel',
+          achternaam: 'Garcia Marquez',
+          geboortedatum: new Date('1927-03-06T00:00:00.000Z'),
+          nationaliteit: 'Colombian',
+          biografie: 'Known for One Hundred Years of Solitude.',
+        },
+      });
+      
+      expect(response.status).toBe(200); 
+      expect(response.body.id).toBeTruthy(); 
+      expect(response.body.titel).toBe('ISBN10'); 
+      expect(response.body.cover_uri).toBe('https://example.com/cover/pride-and-prejudice.jpg'); 
+      expect(response.body.ISBN).toBe('9781590302255');
       expect(response.body.auteur).toEqual(expect.objectContaining({
         voornaam: 'Gabriel',
         achternaam: 'Garcia Marquez',
