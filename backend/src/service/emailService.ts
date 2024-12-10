3e6b62b95aed51dba7850745e3f5181ab0cac5eb
MaximBauwelinck1 <maxim.bauwelinck@student.hogent.be>
2024-12-10 10:39:21 +0100

    render fix hopelijk

--- backend/src/service/emailService.ts
+++ backend/src/service/emailService.ts
@@ -2,6 +2,7 @@ import {transporter} from '../core/nodemailer';
 import ejs from 'ejs';
 import path from 'path';
 import config from 'config';
+import fs from 'fs';
 
 export const sendEmail = async <T extends ejs.Data>(
   to: string,
@@ -9,7 +10,46 @@ export const sendEmail = async <T extends ejs.Data>(
   templateName: string,
   data: T,
 ): Promise<any> => {
+  const templatePath = path.join(__dirname, '../utils/templates', `${templateName}.ejs`);
+
   try {
+    if (!fs.existsSync(templatePath) && templateName === 'bevestigingReservatie') {
+      const defaultTemplate = `
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Bevestiging reservatie</title>
+          </head>
+          <body>
+            <h1>Hallo, <%= voornaam %>, <%= achternaam %></h1>
+            <p>Je hebt zonet een boek uitgeleend. Alle informatie wordt hieronder nog eens opgelijst voor je:</p>
+            <p><b>Titel boek: </b><%= boek_titel %></p>
+            <p><b>ISBN: </b><%= ISBN %></p>
+            <p><b>Uiterste inleverdatum: </b><%= einddatum %></p>
+            <p>Veel leesplezier toegewenst.</p>
+          </body>
+        </html>
+      `;
+      fs.writeFileSync(templatePath, defaultTemplate);
+    } else if (!fs.existsSync(templatePath) && templateName === 'passwordReset'){
+      const defaultTemplate = `
+       <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Password Reset</title>
+        </head>
+        <body>
+          <h1>Hallo, <%= voornaam %>, <%= achternaam %>!</h1>
+          <p>Je hebt een nieuw wachtwoord aangevraagd. Klik op de onderstaande link:</p>
+          <a href="<%= link %>">Reset Wachtwoord</a>
+          <p>Dit veroek vervalt binnen 1uur.</p>
+          <p>Als je hier niet om hebt gevraagd, negeer deze e-mail dan.</p>
+        </body>
+        </html>
+
+      `;
+      fs.writeFileSync(templatePath, defaultTemplate);
+    }
     const emailTemplate = await ejs.renderFile(
       path.join(__dirname, '../utils/templates', `${templateName}.ejs`),
       data,
